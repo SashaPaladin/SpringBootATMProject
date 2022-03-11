83d0b2c5c5f54de9d35edc4a1c48a2ba27eb7c80
SashaPaladin <Alexandro_primachenko@mail.ru>
2022-03-12 00:33:03 +0300

    Initial commit

---
+++ src/main/java/ru/sbrf/springbootatmproject/SpringBootAtmProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.sbrf.springbootatmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAtmProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAtmProjectApplication.class, args);
+    }
+
+}
